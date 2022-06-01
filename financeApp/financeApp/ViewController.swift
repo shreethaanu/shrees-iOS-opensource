fa4b5cfd7e5ac3f19cfc73a85fdfcd939e5ba0e9
ShreeThaanu Raveendran <shree96thaanu@gmail.com>
2022-06-01 23:59:21 +0530

    initial commit for finance app

---
+++ financeApp/financeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  financeApp
+//
+//  Created by shree thaanu on 01/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
